f044e5e1401e3876958c1c31a4f75eb90020a886
Diegus_15 <diegoalexandergj@ufps.edu.co>
2025-04-29 07:31:31 -0500

    commit

---
+++ js/script.js
@@ -0,0 +1,25 @@
+const tarjetas = document.getElementById('tarjetas')
+
+fetch('..json/datos.json')
+.then(response => response.json())
+.then(data =>{
+    data.forEach(foto =>{
+
+        const tarjeta = document.createElement('div');
+        tarjeta.classList.add('tarjeta')
+
+        const  imagen = document.createElement('img')
+        imagen.src = foto.img
+        imagen.alt = foto.description
+
+        const descripcion = document.createElement('p')
+        descripcion.textContent = foto.descripcion
+
+        tarjeta.appendChild(imagen)
+        tarjeta.appendChild(descripcion)
+
+        tarjetas.appendChild(tarjeta)
+    })
+} 
+
+)
